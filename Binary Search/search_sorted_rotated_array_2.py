e29af9088a3628b33b570b06f13aecbad1435678
Arpit <arpitkumar3203@gmail.com>
2023-02-15 22:42:37 +0530

    Added second version of searching in rotated sorted array

---
+++ Binary Search/search_sorted_rotated_array_2.py
@@ -0,0 +1,29 @@
+arr = [1,1,1,1,1,1,1,1,1,1,1,2,1,1,1]
+target = 2
+n = len(arr)
+def find_occurence(arr, n, target):
+    l, r = 0, n-1
+    while l <= r:
+        m = (l+r) // 2
+
+        if target == arr[m]:
+            print(m)
+            return True
+
+        if arr[m] == arr[l] and arr[m] == arr[r]:
+            l += 1
+            r -= 1
+        elif arr[l] <= arr[m]: # from mid to r it is sorted
+            if target < arr[m] and arr[l] <= target:
+                r = m-1
+            else:
+                l = m+1
+        else: # from l to mid it is sorted
+            if target > arr[m] and arr[r] >= target: 
+                l = m+1
+            else:
+                r = m-1 
+
+    return False
+
+print(find_occurence(arr, n, target))
