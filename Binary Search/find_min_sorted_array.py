a0804f0767d2fb9089776bd33b712c89e560edbe
Arpit <arpitkumar3203@gmail.com>
2023-02-17 09:46:39 +0530

    Added changes

--- Binary Search/find_min_sorted_array.py
+++ Binary Search/find_min_sorted_array.py
@@ -4,21 +4,21 @@ def find_min(arr, n):
     l, r = 0, n-1
     res = 1e10
 
-    # in case of no rotation
-    if arr[l] <= arr[r]:
-        return arr[0]
-
     while l <= r:
         m = (l + r) // 2
 
+        # if we found left to right sorted array
         if arr[l] <= arr[r]:
             return min(res, arr[l])
         
         res = min(res, arr[m])
-        if arr[m] >= arr[l]:
+        if arr[m] >= arr[l]: # for this case min could be at left(already took care of it) or to the left side of mid
             l = m+1
         else:
-            r = m-1
+            r = m-1# in case of no rotation
+    if arr[l] <= arr[r]:
+        return arr[0]
+
     return res
 
 print(find_min(arr, n))
