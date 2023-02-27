3ec60574b3f1737efa64d79969b325f3d216192c
Arpit <arpitkumar3203@gmail.com>
2023-02-27 08:38:31 +0530

    Added comb sum 1

---
+++ Recursion/combination_sum_1.py
@@ -0,0 +1,24 @@
+def backtrack(i, arr, curr, res, n, target, currsumm):
+    if currsumm > target or i == n:
+        return
+    
+    if currsumm == target:
+        res.append(curr.copy())
+        return
+    
+    # pick
+    currsumm += arr[i]
+    curr.append(arr[i])
+    backtrack(i, arr, curr, res, n, target, currsumm)
+
+    # not pick
+    currsumm -= arr[i]
+    curr.pop()
+    backtrack(i+1, arr, curr, res, n, target, currsumm)
+
+    return res
+
+arr = [2,3,4,5]
+target = 8
+
+print(backtrack(0, arr, [], [], len(arr), target, 0))
