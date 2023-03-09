1851c85989a10e555073b11d02b1542e18ca912b
Arpit <arpitkumar3203@gmail.com>
2023-03-09 21:08:19 +0530

    added comment

--- Recursion/combination_sum_2.py
+++ Recursion/combination_sum_2.py
@@ -16,15 +16,17 @@ def backtrack(i, arr, curr, res, n, target, currsumm):
 
         currsumm += arr[ind]
         curr.append(arr[ind])
-        backtrack(ind+1, arr, curr, res, n ,target, currsumm)
+        backtrack(ind, arr, curr, res, n ,target, currsumm)
 
         currsumm -= arr[ind]
         curr.pop()
+        # backtrack(ind+1, arr, curr, res, n ,target, currsumm)
+
 
     return res
 
-arr = [4, 19, 30, 11]
+arr = [7,2,6,5]
 arr.sort()
-target = 30
+target = 16
 
 print(backtrack(0, arr, [], [], len(arr), target, 0))
