bf893bee716b41252a08700de76027301d35ff01
Arpit <arpitkumar3203@gmail.com>
2023-03-11 11:43:00 +0530

    Added subset problems

---
+++ Recursion/subsetSum.py
@@ -0,0 +1,20 @@
+def backtrack(i, res, currsum, arr, n):
+    if i == n:
+        res.append(currsum)
+        # print(curr)
+        return
+    
+    # pick
+    currsum += (arr[i])
+    backtrack(i+1, res, currsum, arr, n)
+
+    # not_pick
+    currsum -= arr[i]
+    backtrack(i+1, res, currsum, arr, n)
+
+    # return
+    # res.append(0)
+    return res
+
+arr = [1,2,3]
+print(*backtrack(0, [], 0, arr, len(arr)))
---
+++ Recursion/subsetSum2_subset2.py
@@ -0,0 +1,24 @@
+def backtrack(i, res, curr, arr, n):
+    if i == n:
+        res.append(curr.copy())
+        # print(curr)
+        return
+        
+    # pick
+    curr.append(arr[i])
+    backtrack(i+1, res, curr, arr, n)
+
+    # not_pick
+    while True:
+        if i < n-1 and arr[i] == arr[i+1]:
+            i += 1
+        else:
+            break
+    curr.pop()
+    backtrack(i+1, res, curr, arr, n)
+
+    # return
+    return res
+
+arr = [1,2,2]
+print(backtrack(0, [], [], arr, len(arr)))
