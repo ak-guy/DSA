d0c94069662ec7956f5e5d2b6df5a64f603f7e8d
Arpit <arpitkumar3203@gmail.com>
2023-04-19 10:42:14 +0530

    added complete solution of vertical order traversal

--- Binary Tree/vertical_order_traversal.py
+++ Binary Tree/vertical_order_traversal.py
@@ -1,37 +1,35 @@
-# Could not handle this case :
-# # There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values.
-
-
 from collections import defaultdict,deque
 class Solution:
     def verticalTraversal(self, root):
         # dic = {} -> vertical_level(integer) : root_val(list)
         minn_level = 1e10
         maxx_level = -1e10
-
         def levelorder(root):
             nonlocal minn_level
             nonlocal maxx_level
             res = []
             q = deque()
-            q.append((root, 0))
-            dic = defaultdict(list)
+            q.append((root, 0, 0)) # root : level or row or height : column or vertical_height 
+            dic = defaultdict(lambda : defaultdict(list))
 
             while q:
-                dummy_dict = defaultdict(list)
                 for i in range(len(q)):
-                    node, vals = q.popleft()
+                    node, level, col = q.popleft()
                     if node:
-                        minn_level = min(minn_level, vals)
-                        maxx_level = max(maxx_level, vals)
-                        dummy_dict[str(vals)].append(node.val)
-                        q.append((node.left, vals-1))
-                        q.append((node.right, vals+1))
+                        minn_level = min(minn_level, col)
+                        maxx_level = max(maxx_level, col)
+                        dic[str(col)][level].append(node.val)
+                        dic[str(col)][level].sort()
+                        q.append((node.left, level+1, col-1))
+                        q.append((node.right, level+1, col+1))
             return dic
 
         ans = levelorder(root)
         res = []
         for i in range(minn_level, maxx_level+1):
-            res.append(ans[str(i)])
+            dummy = []
+            for level in ans[str(i)]:
+                dummy.extend(ans[str(i)][level])
+            res.append(dummy)
             
         return res
