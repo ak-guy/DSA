e21b1f4396c1e633579ffff36f58fae7ed3dffe7
Arpit <arpitkumar3203@gmail.com>
2023-04-21 10:14:45 +0530

    added right view of binary tree

---
+++ Binary Tree/right_view.py
@@ -0,0 +1,44 @@
+# Using Recursion
+class Solution:
+    def rightSideView(self, root):
+        
+        res = []
+        
+        def dfs(root, level):
+            if not root:
+                return 
+            
+            if level == len(res):
+                res.append(root.val)
+                
+            dfs(root.right, level+1)
+            dfs(root.left, level+1)
+        
+        
+        dfs(root, 0)
+        return res
+    
+
+# Without using recursion 
+from collections import deque
+class Solution:
+    def rightSideView(self, root):
+        def sol(root):
+            q = deque()
+            q.append(root)
+            res = []
+            
+            while q:
+                len_q = len(q)
+                for i in range(len_q):
+                    node = q.popleft()
+                    if node:
+                        if i == len_q-1:
+                            res.append(node.val)
+                        if node.left:
+                            q.append(node.left)
+                        if node.right:
+                            q.append(node.right)
+            return res
+        res = sol(root)
+        return res
