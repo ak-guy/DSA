eb1475c4e4f50a5864193748e4cbf2ae0d38de57
Arpit <arpitkumar3203@gmail.com>
2023-05-02 22:47:56 +0530

    Added construct binary tree from postorder and inorder

---
+++ Binary Tree/construct_tree_from_post_ino.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def buildTree(self, inorder, postorder):
+        postorder = postorder[::-1]
+
+        def sol(post, ino):
+            if not post and not ino:
+                return None
+
+            mid_val = ino.index(post[0])
+            root = TreeNode(post[0])
+
+            root.left = sol(post[len(post)-mid_val:], ino[:mid_val])
+            root.right = sol(post[1:len(post)-mid_val], ino[mid_val+1:])
+
+            return root
+
+        return sol(postorder, inorder)
