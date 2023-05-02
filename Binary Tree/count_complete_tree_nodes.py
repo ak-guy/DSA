329430f963e2f3d34e4662c3ab14eb82b18cbbb2
Arpit <arpitkumar3203@gmail.com>
2023-05-02 09:02:02 +0530

    Added count complete tree nodes and construct tree from pre and inorder

---
+++ Binary Tree/count_complete_tree_nodes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkHeight(self, root):
+        def leftHeight(root):
+            if not root:
+                return 0
+            
+            return 1 + leftHeight(root.left)
+
+        def rightHeight(root):
+            if not root:
+                return 0
+
+            return 1 + rightHeight(root.right)
+
+        return [leftHeight(root) == rightHeight(root), leftHeight(root)]
+        
+    def countNodes(self, root) -> int:
+        
+        def sol(root):
+            if not root:
+                return 0
+
+            val = self.checkHeight(root)
+            if val[0]:
+                return 2 ** val[1] - 1
+
+            left = sol(root.left)
+            right = sol(root.right)
+
+            return 1 + left + right
+
+        return sol(root)
