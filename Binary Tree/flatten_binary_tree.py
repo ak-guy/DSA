467eaea85b26c65bc70113945f8e129a32115981
Arpit <arpitkumar3203@gmail.com>
2023-05-16 07:24:58 +0530

    Added changes in flatten binary tree

---
+++ Binary Tree/flatten_binary_tree.py
@@ -0,0 +1,18 @@
+# # Method 1 => Using Recuesion
+
+class Solution:
+    def flatten(self, root):
+        prev_node = None
+        def sol(root):
+            nonlocal prev_node
+            if not root:
+                return
+
+            sol(root.right)
+            sol(root.left)
+            
+            root.right = prev_node
+            root.left = None
+            prev_node = root
+
+        sol(root)
