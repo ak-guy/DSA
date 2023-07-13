8a5b35ece62c14eaea2c94ea41ae8b1c4fd4518c
Arpit <arpitkumar3203@gmail.com>
2023-07-13 08:08:17 +0530

    added minimum_insertion_or_deletion_to_make_two_string_equal

---
+++ Dynamic Programming/String DP/minimum_insertion_or_deletion_to_make_two_string_equal.py
@@ -0,0 +1,25 @@
+# # end result will depend on longestCommonSubsequence possible for given two strings
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+
+        dp = [[0 for i in range(m+1)] for j in range(n+1)]
+
+        # have already initialized the base case while creating dp array
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                match = 0
+                not_match = 0
+                if text1[i-1] == text2[j-1]:
+                    match = 1 + dp[i-1][j-1]
+                else:
+                    not_match = max(dp[i][j-1], dp[i-1][j])
+
+                dp[i][j] = match+not_match
+
+        return dp[n][m]
+    
+    def minDistance(self, word1: str, word2: str) -> int:
+        return len(word1)+len(word2) - 2 * (self.longestCommonSubsequence(word1, word2))
