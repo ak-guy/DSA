b4db2b53d49a647bdd642018b5ad4b2b9ecb7437
Arpit <arpitkumar3203@gmail.com>
2023-08-09 08:34:53 +0530

    added count all square submatrices

---
+++ Dynamic Programming/Square DP/count_all_square_submatrices.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSquares(self, mat) -> int:
+        n = len(mat)
+        m = len(mat[0])
+        res = 0
+        for r in range(n):
+            for c in range(m):
+                if mat[r][c] == 1 and r>0 and c>0:
+                    mat[r][c] += min(mat[r][c-1], mat[r-1][c-1], mat[r-1][c])
+                res += mat[r][c]
+
+        return res
