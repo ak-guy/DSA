99587e97545f0067e1a9df825ead5d219153c224
Arpit <arpitkumar3203@gmail.com>
2023-08-10 08:54:15 +0530

    added sqrt of a number and changed directory of few BS problems

---
+++ Binary Search/BS on Answer/sqrt.py
@@ -0,0 +1,16 @@
+class Solution:
+    def floorSqrt(self, x):
+        if x == 0:
+            return 0
+        l = 1
+        r = x
+        ans = 1
+        while l <= r:
+            m = (l+r) // 2
+            if (m*m) <= x:
+                ans = m
+                l = m+1
+            else:
+                r = m-1
+        
+        return ans # can return either ans or r
