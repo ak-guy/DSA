3e94691963f88034f48b3f4204115837463902d9
Arpit <arpitkumar3203@gmail.com>
2023-08-11 08:52:05 +0530

    added koko eating bananas

---
+++ Binary Search/BS on Answer/koko_eating_bananas.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def Solve(self, N, piles, h): # O(nlog(max(piles)))
+        l = 1
+        r = res = max(piles)
+        n = len(piles)
+
+        while l <= r:
+            mid = (l+r) // 2
+
+            hours = 0
+            for i in range(n):
+                hours += math.ceil(piles[i] / mid)
+            if hours <= h:
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return res
