5780420e299b290fe86237b007652267e756bc6b
Arpit <arpitkumar3203@gmail.com>
2023-08-14 23:26:09 +0530

    added split array largest sum

---
+++ Binary Search/BS on Answer/split_array_largest_sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getPossibility(self, arr, search_summ, k, n):
+        rem_search_summ = search_summ
+        for i in range(n):
+            if arr[i] <= rem_search_summ:
+                rem_search_summ -= arr[i]
+            else:
+                k -= 1
+                rem_search_summ = search_summ
+                rem_search_summ -= arr[i]
+        
+        return k >= 1
+
+    def splitArray(self, nums, k: int) -> int:
+        l, r = max(nums), sum(nums)
+        res = 10000000000
+        n = len(nums)
+
+        while l <= r:
+            mid = (l+r) // 2
+            cond = self.getPossibility(nums, mid, k, n) # assuming this is the max sum we want array to have
+
+            if cond:
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+            
+        return res
