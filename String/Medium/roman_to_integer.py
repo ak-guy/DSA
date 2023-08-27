f2d7fe33e845949023d8d2374159dcc0b60e36f2
Arpit <arpitkumar3203@gmail.com>
2023-08-27 10:47:20 +0530

    added roman to integer

---
+++ String/Medium/roman_to_integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToDecimal(self, S):
+        res = 0
+        mapping = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        S = S.replace("IV", "IIII").replace("IX", "VIIII")
+        S = S.replace("XL", "XXXX").replace("XC", "LXXXX")
+        S = S.replace("CD", "CCCC").replace("CM", "DCCCC")
+        
+        for char in S:
+            res += mapping[char]
+        
+        return res
