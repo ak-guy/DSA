9187bc84da2359e9472758f554b115cd12d53a6a
Arpit <arpitkumar3203@gmail.com>
2023-09-01 23:29:05 +0530

    added similar problem's solution from gfg

--- Misc/previous_permutation_with_one_swap.py
+++ Misc/previous_permutation_with_one_swap.py
@@ -1,5 +1,5 @@
 # similar concept as Next permutation
-class Solution:
+class Solution: # # Leetcode (previous permutation with one swap)
     def prevPermOpt1(self, arr):
         n = len(arr)
         bp = -1
@@ -18,3 +18,29 @@ class Solution:
                 break
 
         return arr
+    
+
+class Solution: # # GFG (Previous number in one swap)
+    def previousNumber (ob,S):
+        arr = [int(i) for i in S]
+        n = len(arr)
+        bp = -1
+        for i in range(n-2, -1, -1):
+            if arr[i+1] < arr[i]:
+                bp = i
+                break
+            
+        if bp == -1:
+            return "-1"
+
+        res_ind = -1
+        for i in range(n-1, bp-1, -1):
+            if arr[bp] > arr[i] and (i>0 and arr[i] != arr[i-1]): # this cond -> (i>0 and arr[i] != arr[i-1]) is because there can exist duplicate, so we will pick last duplicate elem present if we start traversing from back  
+                arr[bp], arr[i] = arr[i], arr[bp]
+                break
+            
+        res = "".join(str(val) for val in arr)
+        if res[0] == '0':
+            return "-1"
+        else:
+            return res
