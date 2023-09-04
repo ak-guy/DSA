30725438e1ab69dc43f2111dcb4f5a437caceb42
Arpit <arpitkumar3203@gmail.com>
2023-09-04 23:54:28 +0530

    added reverse each word in given string

---
+++ String/Medium/reverse_each_word_in_given_string.py
@@ -0,0 +1,9 @@
+# # GFG Problem
+class Solution:
+    def reverseWords(self, s):
+        arr = list(s.split("."))
+        for i in range(len(arr)):
+            curr = arr[i]
+            arr[i] = curr[::-1]
+        
+        return ".".join(arr)
