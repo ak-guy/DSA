b869101d636666ed57cd3f87bd801843d172c622
Arpit <arpitkumar3203@gmail.com>
2023-09-04 23:56:14 +0530

    added solution using dictionary

--- String/Medium/sum_beauty_of_all_strings.py
+++ String/Medium/sum_beauty_of_all_strings.py
@@ -1,3 +1,4 @@
+# using array
 class Solution:
     def beautySum(self, s: str) -> int:
         n = len(s)
@@ -15,4 +16,16 @@ class Solution:
                         least = min(least, char_count[k])
                 res += (most - least)
                   
+        return res
+
+# Using dictionary   
+class Solution:
+    def beautySum(self, s):
+        n = len(s)
+        res = 0
+        for i in range(n):
+            char_count = {}
+            for j in range(i, n):
+                char_count[s[j]] = 1 + char_count.get(s[j], 0)
+                res += (max(char_count.values()) - min(char_count.values()))
         return res
