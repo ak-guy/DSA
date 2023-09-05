5dbfa61c8b1ac08cdee09f6b7059e6f589d59b49
Arpit <arpitkumar3203@gmail.com>
2023-09-05 23:17:02 +0530

    added infix to postfix using stack

---
+++ Stacks and Queues/Pre In Post Convention/infix_to_postfix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def InfixtoPostfix(self, exp):
+        res = ""
+        st = []
+        precedence_mapping = {"^":4, "*":3, "/":2, "+":1, "-":1, "(":0}
+        for char in exp:
+            if char.isalnum(): # if char is alphabet or number we can directly add in res
+                res += char
+            else:
+                if char == '(': # if char is '(' then append it in stack
+                    st.append(char)
+                elif char == ')': # if char is ')' then pop from stack till and including '('
+                    while st[-1] != '(':
+                        res += st.pop()
+                    st.pop() # to remove '('
+                else: # if char is some operator
+                    while st and precedence_mapping[char] <= precedence_mapping[st[-1]]: # we need to pop all operator which have higher precedence
+                        res += st.pop()
+                    st.append(char) # adding the operator after poping all higher precedence operator
+        
+        while st: # for the remaining operator in stack
+            res += st.pop()
+        
+        return res
