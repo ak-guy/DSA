5160cb7ab19585f35c2e70f468dc683ff70bfa2d
Arpit <arpitkumar3203@gmail.com>
2023-09-11 10:45:39 +0530

    added next greater element 2

--- Stacks and Queues/Monotonic Stack and Queue/next_greater_element.py
+++ Stacks and Queues/Monotonic Stack and Queue/next_greater_element.py
@@ -1,4 +1,8 @@
+# # Leetcode
 class Solution:
+    '''
+    idea is to move in reverse on nums2 and maintain a monotonic inc stack
+    '''
     def nextGreaterElement(self, nums1, nums2):
         st = [] # this will be our monotonically increasing stack, st[-1] < st[-2]
         hmap = {val:ind for ind, val in enumerate(nums1)}
@@ -17,6 +21,27 @@ class Solution:
                     nums1[hmap[val]] = res
                     st.append(res) # adding to stack the poped value
 
+            st.append(val)
+        
+        return nums1
+    
+
+# # GFG
+class Solution:
+    def nextLargerElement(self,nums2,n):
+        nums1 = [-1 for i in range(n)] 
+        st = [] # this will be our monotonically increasing stack, st[-1] < st[-2]
+        
+        for i in range(len(nums2)-1, -1, -1):
+            val = nums2[i]
+            while st and st[-1] <= val:
+                st.pop()
+            
+            if st:
+                res = st.pop()
+                nums1[i] = res
+                st.append(res)
+                
             st.append(val)
         
         return nums1
---
+++ Stacks and Queues/Monotonic Stack and Queue/next_greater_element_2.py
@@ -0,0 +1,27 @@
+'''
+suppose nums = [5,4,3,2,1]
+then modified_nums = [5,4,3,2,1,5,4,3,2,1]
+now if we find NGE of modified_nums and take only first 5 elements then we will get result
+but we dont need to append nums again, we can mimic that by traversing from 2*n - 1 to 0 index and take
+current_value as nums[i%n]
+'''
+from typing import List
+class Solution:
+    def nextGreaterElements(self, nums2: List[int]) -> List[int]:
+        n = len(nums2)
+        nums1 = [-1 for i in range(n)] 
+        st = [] # this will be our monotonically increasing stack, st[-1] < st[-2]
+        
+        for i in range(2 * len(nums2) -1, -1, -1):
+            val = nums2[i%n]
+            while st and st[-1] <= val:
+                st.pop()
+            
+            if st:
+                res = st.pop()
+                nums1[i%n] = res
+                st.append(res)
+                
+            st.append(val)
+        
+        return nums1
