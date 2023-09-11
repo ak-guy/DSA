ea134c46e0b3e893fe12957f7bd9ccd66224ea89
Arpit <arpitkumar3203@gmail.com>
2023-09-11 23:01:33 +0530

    added number of next greater elements on right

---
+++ Stacks and Queues/Monotonic Stack and Queue/number_of_nextgreaterelements_on_the_right.py
@@ -0,0 +1,13 @@
+# # Brute Force
+class Solution: # TC -> O(N*queries) and SC -> O(queries)
+    def count_NGEs(self, N, arr, queries, indices):
+        ans = [0 for i in range(queries)]
+        for i in range(queries):
+            ind = indices[i]
+            res = 0
+            for j in range(ind+1, N):
+                if arr[j] > arr[ind]:
+                    res += 1
+            ans[i] = res
+        
+        return ans
