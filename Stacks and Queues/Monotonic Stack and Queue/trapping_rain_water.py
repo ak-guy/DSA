fbcbf93b2029567d987fb4bf3378e4a852cc25f4
Arpit <arpitkumar3203@gmail.com>
2023-09-12 22:43:03 +0530

    added optimized two pointer approach for trapping rainwater problem

--- Stacks and Queues/Monotonic Stack and Queue/trapping_rain_water.py
+++ Stacks and Queues/Monotonic Stack and Queue/trapping_rain_water.py
@@ -20,3 +20,29 @@ class Solution: # TC -> O(N) and SC -> O(N)
         
         return main_res
     
+# # Method - 2 (Using Two Pointer)
+class Solution:
+    def trappingWater(self, arr,n):
+        main_res = 0
+        l, r = 0, n-1
+        left_max = 0 # this will tell what is (one of the) max height of building to the left of index i 
+        right_max = 0 # this will tell what is (one of the) max height of building to the right of index i
+        
+
+        ''' 
+        for brute force we were using -> main_res += (min(prefix_height[i], suffix_height[i]) - arr[i])
+        but here if we know what which one is minimum max_height_left_building or max_height_right_building
+        then we can directly add that result to main_res
+
+        '''
+        while l <= r:
+            if arr[l] <= arr[r]:
+                left_max = max(left_max, arr[l])
+                main_res += (left_max - arr[l]) # we add like this in res because we are sure that the max_height of builing on the left will be atleast of length arr[l]
+                l += 1
+            else:
+                right_max = max(right_max, arr[r])
+                main_res += (right_max - arr[r]) # we add like this in res because we are sure that the max_height of builing on the right will be greater than arr[r]
+                r -= 1
+        
+        return main_res
