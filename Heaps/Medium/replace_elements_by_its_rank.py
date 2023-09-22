447bc98f0a6974fd497a1a37add5cfae84039eaa
Arpit <arpitkumar3203@gmail.com>
2023-09-22 08:06:27 +0530

    added replace elements by its rank

---
+++ Heaps/Medium/replace_elements_by_its_rank.py
@@ -0,0 +1,34 @@
+# # Method - 1 (Using sorted function)
+class Solution:
+    def replaceWithRank(self, N, arr):
+        new_arr = sorted(arr)
+        rank = 1
+        dic = {}
+        for i in range(N):
+            if new_arr[i] not in dic:
+                dic[new_arr[i]] = rank
+                rank += 1
+        
+        for i in range(N):
+            arr[i] = dic[arr[i]]
+        
+        return arr
+    
+# # Method - 2 (Using heaps)
+import heapq
+class Solution:
+    def replaceWithRank(self, N, arr):
+        hq = []
+        rank = 0
+        for i in range(N):
+            heapq.heappush(hq, (arr[i], i))
+        
+        prev_val = 0
+        while hq:
+            val, i = heapq.heappop(hq)
+            if val != prev_val:
+                rank += 1
+            arr[i] = rank
+            prev_val = val
+        
+        return arr
