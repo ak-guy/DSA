f1473fe4e9d8005a1c8b7575b6d72cc9e60d4de7
Arpit <arpitkumar3203@gmail.com>
2023-10-08 21:08:23 +0530

    added algo for hands of straight

--- Heaps/Medium/hands_of_straight.py
+++ Heaps/Medium/hands_of_straight.py
@@ -2,6 +2,11 @@ import heapq
 from collections import Counter
 from typing import List
 class Solution:
+    '''
+    will use heapify all unique val in arr, then pop the first smallest val and run a loop from that val
+    to (val+groupsize) and if any number is not present then we can return False else we can handle dict
+    and heap accordingly
+    '''
     def isPossibleDivide(self, hand: List[int], groupSize: int) -> bool:
         dic = Counter(hand)
         hq = [val for val in dic.keys()]
