4ad07a401d928f5e62fd88f4696db6db6a475dd8
Arpit <arpitkumar3203@gmail.com>
2023-10-09 22:34:22 +0530

    added algo for postf to inf

--- Stacks and Queues/Pre In Post Convention/postfix_to_infix.py
+++ Stacks and Queues/Pre In Post Convention/postfix_to_infix.py
@@ -1,4 +1,9 @@
 class Solution:
+    '''
+    same as pref to inf, make a stack, traverse in reverse, will store res in stack so final ans will be st.pop()
+    if we encounter alnum then directly append it to stack else pop two items and append the
+    appropriate result in stack
+    '''
     def postToInfix(self, postfix):
         st = []
         for char in postfix:
