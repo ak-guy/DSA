5f613aaf73d91f24f7301f1e766f79b7f5de30cc
Arpit <arpitkumar3203@gmail.com>
2023-10-09 09:21:24 +0530

    added algo for kth largest element in stream

--- Heaps/Hard/kth_largest_element_in_stream.py
+++ Heaps/Hard/kth_largest_element_in_stream.py
@@ -1,6 +1,9 @@
 import heapq
 from typing import List
 class KthLargest:
+    '''
+    in this we only need to ensure that heap contains exactly k elements, so that we can return heap[0]
+    '''
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.nums = nums
