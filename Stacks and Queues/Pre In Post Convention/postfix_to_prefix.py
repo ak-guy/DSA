1cc7a4e7ccc9750d425457f5bc4a9fbfbcf414f9
Arpit <arpitkumar3203@gmail.com>
2023-10-09 22:47:30 +0530

    added algo for postf to pref

--- Stacks and Queues/Pre In Post Convention/postfix_to_prefix.py
+++ Stacks and Queues/Pre In Post Convention/postfix_to_prefix.py
@@ -1,4 +1,9 @@
 class Solution:
+    '''
+    make a stack, traverse in **forward**, will store res in stack, if alnum append in stack,
+    else if operator is encountered then st.append(str(operator+second+first))
+    atlast return st.pop()
+    '''
     def postToPre(self, post_exp):
         st = []
         for char in post_exp:
