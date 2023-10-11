b87ead77033e4db79b0184174e4ab4b976373ea6
Arpit <arpitkumar3203@gmail.com>
2023-10-11 09:12:53 +0530

    added algo for remove k digits

--- Stacks and Queues/Monotonic Stack and Queue/remove_k_digits.py
+++ Stacks and Queues/Monotonic Stack and Queue/remove_k_digits.py
@@ -1,4 +1,10 @@
 class Solution:
+    '''
+    take example of '123456789' we have to remove 3 digits which one should we remove -> 789
+    take example of '987654321' we have to remove 3 digits which one should we remove -> 987
+    so we need to maintain a monotonic inc stack and and whenever we find a number that is
+    smaller than st[-1] we will pop that number
+    '''
     def removeKdigits(self, S, K):
         st = []
         for i in range(len(S)):
