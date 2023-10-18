b51458ac70c0a1b4fddccc2665c18c5d0093446a
Arpit <arpitkumar3203@gmail.com>
2023-10-18 22:28:09 +0530

    added algo for peak element

--- Binary Search/BS on 1D/find_peak_element.py
+++ Binary Search/BS on 1D/find_peak_element.py
@@ -1,3 +1,9 @@
+'''
+peak element, arr[i], is an element such that arr[i-1] < arr[i] > arr[i+1]
+every array will always have atleast one peak element
+suppose we have an index m = 5, then we check arr[5] > arr[6] if this condition is true then 
+it means arr[5] can be a peak element so we will shift right_pointer to index 5 else left_pointer to 6
+'''
 def peakElement(arr, n):
     left, right = 0, n-1
     while left < right:
