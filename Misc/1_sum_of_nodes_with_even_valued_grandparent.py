7821f752192c0c2a4e13b407b049b4d0cb6ab36e
Arpit <arpitkumar3203@gmail.com>
2023-10-23 22:13:33 +0530

    added sum_of_nodes_with_even_valued_grandparent

---
+++ Misc/1_sum_of_nodes_with_even_valued_grandparent.py
@@ -0,0 +1,45 @@
+# # Method - 1 (Brute Force)
+
+
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def helper(self, root):
+        # arr = [root]
+        value = 0
+        count = 2
+        q = deque()
+        q.append(root)
+        while q and count:
+            n = len(q)
+            for i in range(n):
+                _root = q.popleft()
+                if _root.left:
+                    q.append(_root.left)
+                if _root.right:
+                    q.append(_root.right)
+            count -= 1
+
+        while q:
+            value += q.pop().val
+        return value
+
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        res = 0
+        def traverse(root):
+            nonlocal res
+            if not root:
+                return
+            traverse(root.left)
+            traverse(root.right)
+
+            if root and root.val % 2 == 0:
+                res += self.helper(root)
+            
+        traverse(root)
+        return res
