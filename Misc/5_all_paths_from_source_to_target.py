eabdec00ac962641885dea18aa4eda81aa37b5a5
Arpit <arpitkumar3203@gmail.com>
2023-10-25 08:44:39 +0530

    added dfs solution for all paths from source to target

--- Misc/5_all_paths_from_source_to_target.py
+++ Misc/5_all_paths_from_source_to_target.py
@@ -20,3 +20,18 @@ class Solution:
         
 
 # # Method - 2 (DFS Iterative)
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        res = []
+        n = len(graph)
+
+        def recursive(path):
+            if path[-1] == n-1:
+                res.append(path)
+                return
+            else:
+                for val in graph[path[-1]]:
+                    recursive(path + [val])
+        
+        recursive([0])
+        return res
