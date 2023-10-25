17412bc1e6ebd6d7681ce0bd3b3fa8217fe6c523
Arpit <arpitkumar3203@gmail.com>
2023-10-25 23:36:31 +0530

    added find triangular sum in array

---
+++ Misc/6_find_triangular_sum_in_array.py
@@ -0,0 +1,13 @@
+# # Method - 1 (Brute Force)
+from typing import List
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        for r in range(n-1,0,-1):
+            for c in range(r):
+                nums[c] = (nums[c] + nums[c+1]) % 10
+        
+        return nums[0]
+    
+
+# # Method - 2 (Maths -> Pascal Triangle concept)
