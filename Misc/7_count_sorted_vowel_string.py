fc74448d1a4f91e84535b65d77934a45f5ff9710
Arpit <arpitkumar3203@gmail.com>
2023-10-27 08:17:07 +0530

    added count sorted vowels string

---
+++ Misc/7_count_sorted_vowel_string.py
@@ -0,0 +1,20 @@
+'''
+DP Table :
+
+        a   e   i   o   u
+n=1     5   4   3   2   1
+n=2     15  10  6   3   1
+n=3     35  20  10  4   1
+'''
+
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        dp = [i for i in range(5,0,-1)]
+
+        for k in range(n-1):
+            temp = dp
+            for i in range(3,-1,-1):
+                temp[i] = dp[i] + temp[i+1]
+            dp = temp
+
+        return dp[0] 
