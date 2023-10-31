ab2a45dd8d108c998cb2c88ba1293c121bb447d3
Arpit <arpitkumar3203@gmail.com>
2023-10-31 08:57:38 +0530

    added partition array such that max diff is k

---
+++ Misc/11_partition_array_such_that_maximum_difference_is_k.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort() # O(NlogN)
+        res = 1
+        l, r = 0, 0
+        for i in range(len(nums)):
+            if nums[r] - nums[l] <= k:
+                r += 1
+            else:
+                l = i
+                r = i+1
+                res += 1
+        return res
