73d3de44884927720497f9f356bde75304619596
Arpit <arpitkumar3203@gmail.com>
2023-11-03 23:43:37 +0530

    added reduce array size to half

---
+++ Misc/15_reduce_array_size_to_half.py
@@ -0,0 +1,19 @@
+from typing import List
+import heapq
+from collections import Counter
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        res=0
+        counter = Counter(arr)
+        
+        hq = []
+        for key, val in counter.items():
+            heapq.heappush(hq, (-val,key))
+
+        n = len(arr)
+        max_len = n / 2
+        while n>max_len:
+            count, num = heapq.heappop(hq)
+            n += count
+            res += 1
+        return res
