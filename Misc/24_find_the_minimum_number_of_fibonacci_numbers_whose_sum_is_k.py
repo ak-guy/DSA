88aa92f9a95d36ec40e38d9d00445e65cd23d6a6
ak-guy <arpitkumar3203@gmail.com>
2023-12-22 23:10:48 +0530

    added find the minimum no of fib numbers whose sum is k

---
+++ Misc/24_find_the_minimum_number_of_fibonacci_numbers_whose_sum_is_k.py
@@ -0,0 +1,20 @@
+# Greedy Approach
+class Solution:
+    def getClosestFibonacciNumber(self, value: int) -> int:
+        first = 0
+        second = 1
+        third = 1
+        while third <= value:
+            third = first + second
+            first = second
+            second = third
+
+        return first
+        
+    def findMinFibonacciNumbers(self, k: int) -> int:
+        count = 0
+        while k:
+            k -= self.getClosestFibonacciNumber(k) # we will take the closest fibonacci number to k and substract it with k, then use the remaining part of k to repeat the process 
+            count += 1
+
+        return count
