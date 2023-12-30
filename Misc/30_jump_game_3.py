0657dd8a80f2657f2169e9c01a0d7e809220b81e
ak-guy <arpitkumar3203@gmail.com>
2023-12-30 10:34:59 +0530

    added jump game 3

---
+++ Misc/30_jump_game_3.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        if arr[start] == 0:
+            return True
+        n = len(arr)
+
+        st = []
+        st.append([arr[start], start])
+        arr[start] *= -1
+
+        while st:
+            jump_value, ind = st.pop()
+            if jump_value == 0:
+                return True
+            left_jump = ind - jump_value
+            right_jump = ind + jump_value
+
+            if left_jump >= 0 and arr[left_jump] >= 0:
+                st.append([arr[left_jump], left_jump])
+                arr[left_jump] *= -1
+            if right_jump < n and arr[right_jump] >= 0:
+                st.append([arr[right_jump], right_jump])
+                arr[right_jump] *= -1
+        
+        return False
