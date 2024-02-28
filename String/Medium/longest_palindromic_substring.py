4f606f2ce732427bcaa367b51acda80b3ae9ba39
Arpit <arpitkumar3203@gmail.com>
2024-02-29 01:05:59 +0530

    added more friendly and intuitive solution for longest palindromic substring

--- String/Medium/longest_palindromic_substring.py
+++ String/Medium/longest_palindromic_substring.py
@@ -64,3 +64,46 @@ class Solution:
                 r += 1
         
         return res
+    
+
+# expand from the middle technique
+class Solution:
+    def getLongestPalindrome(self, s: str, starting_index: int, is_odd: bool, string_length: int):
+        ind_1 = starting_index-1
+        ind_2 = starting_index+1 if is_odd else starting_index+2
+        
+        if not is_odd and s[starting_index] != s[starting_index+1]:
+            return '', 0
+
+        largest_palindrome_length = 1 if is_odd else 2
+        while ind_1 >= 0 and ind_2 <= string_length-1:
+            if s[ind_1] != s[ind_2]:
+                break
+            largest_palindrome_length += 2
+            ind_1 -= 1
+            ind_2 += 1
+
+        return s[ind_1+1:ind_1+largest_palindrome_length+1], largest_palindrome_length
+
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        ans = ''
+        largest_palindrome_length = 0
+        for i in range(n):
+            # odd length palindrome
+            res, res_length = self.getLongestPalindrome(s, starting_index=i, is_odd=True, string_length=n)
+            if res_length > largest_palindrome_length:
+                largest_palindrome_length = res_length
+                ans = res
+
+            # skip checking even length palindromic substring if we reach (n-1)th index
+            if i == n-1:
+                continue
+
+            # even length palindrome
+            res, res_length = self.getLongestPalindrome(s, starting_index=i, is_odd=False, string_length=n)
+            if res_length > largest_palindrome_length:
+                largest_palindrome_length = res_length
+                ans = res
+
+        return ans
