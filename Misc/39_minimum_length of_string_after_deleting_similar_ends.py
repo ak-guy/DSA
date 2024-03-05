56873b8978c3f024b9023fe90b6c5b57e7987f58
Arpit <arpitkumar3203@gmail.com>
2024-03-05 08:55:10 +0530

    added minimum_length of_string_after_deleting_similar_ends.py

---
+++ Misc/39_minimum_length of_string_after_deleting_similar_ends.py
@@ -0,0 +1,17 @@
+# Two pointer
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] == s[r]:
+                if l < r-1 and s[l+1] == s[r]:
+                    l += 1
+                elif r > l+1 and s[r-1] == s[l]:
+                    r -= 1
+                else:
+                    l += 1
+                    r -= 1
+            else:
+                break
+
+        return abs(r-l+1)
