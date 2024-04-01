aabbc38be26b81d96f0570e1c0a883d62d5ae779
Arpit <arpitkumar3203@gmail.com>
2024-04-02 00:45:07 +0530

    added subarray product less than k

---
+++ Misc/50_subarray_product_less_than_k.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        res = 0
+        if k == 0:
+            return res
+        
+        n = len(nums)
+        sliding_window_starting_index = 0
+        curr_product = 1
+        for ind in range(n):
+            curr_product *= nums[ind]
+            while curr_product >= k and sliding_window_starting_index <= ind:
+                curr_product /= nums[sliding_window_starting_index]
+                sliding_window_starting_index += 1
+
+            res += (ind - sliding_window_starting_index + 1)
+
+        return res
