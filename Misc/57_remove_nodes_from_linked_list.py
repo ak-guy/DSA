fbdfe4ea9cbdb140c59db3cadfcc40ac5c023de8
Arpit <arpitkumar3203@gmail.com>
2024-04-22 21:30:56 +0530

    added remove nodes from linked list

--- Misc/57_remove_nodes_from_linked_list.py
+++ Misc/57_remove_nodes_from_linked_list.py
@@ -6,7 +6,7 @@ class ListNode:
         self.val = val
         self.next = next
 
-# method 1: using stack and creating a new linked list based on values present in stack
+# Method 1: using stack and creating a new linked list based on values present in stack
 class Solution1:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         st = []
@@ -26,3 +26,46 @@ class Solution1:
             dummy = create_node
 
         return res_node.next.next
+    
+
+# Method - 2: traversing in reversed ll
+class Solution2:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        # reversing
+        curr = head
+        prev = None
+        while curr:
+            temp_next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp_next
+
+        # prev will now be pointing at tail of the original ll
+        newHead = prev
+        temp_prev = prev
+        max_val = prev.val
+        prev = prev.next
+        while prev:
+            while prev and max_val > prev.val:
+                temp_prev.next = prev.next
+                prev = prev.next
+
+            if not prev:
+                break
+
+            max_val = max(max_val, prev.val)
+            temp_prev = prev
+            prev = prev.next
+        
+        # again reverse
+        prev = None
+        while newHead:
+            temp_next = newHead.next
+            newHead.next = prev
+            prev = newHead
+            newHead = temp_next
+
+        return prev
