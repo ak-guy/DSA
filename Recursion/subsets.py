2f4da7b510d75afdb65ee047f26462e672abd291
Arpit <arpitkumar3203@gmail.com>
2024-04-27 11:24:38 +0530

    added subsets 1

---
+++ Recursion/subsets.py
@@ -0,0 +1,23 @@
+# print all subsets, all numbers are unique
+from typing import List
+
+class Solution:
+    def __init__(self):
+        self.res = []
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        def recursion(recursion_depth, subset):
+            nonlocal n
+            if recursion_depth == n:
+                self.res.append(subset.copy())
+                return
+            
+            subset.append(nums[recursion_depth])
+            recursion(recursion_depth + 1, subset)
+            
+            subset.pop()
+            recursion(recursion_depth + 1, subset)
+
+        recursion(0, [])
+        return self.res
