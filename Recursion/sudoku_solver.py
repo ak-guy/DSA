2d12621d1b56a78f6745b345b58ca8539849a417
Arpit <arpitkumar3203@gmail.com>
2024-04-30 01:07:43 +0530

    added sudoku solver

--- Recursion/sudoku_solver.py
+++ Recursion/sudoku_solver.py
@@ -1,45 +1,33 @@
-board = [["5","3",".",".","7",".",".",".","."],
-         ["6",".",".","1","9","5",".",".","."],
-         [".","9","8",".",".",".",".","6","."],
-         ["8",".",".",".","6",".",".",".","3"],
-         ["4",".",".","8",".","3",".",".","1"],
-         ["7",".",".",".","2",".",".",".","6"],
-         [".","6",".",".",".",".","2","8","."],
-         [".",".",".","4","1","9",".",".","5"],
-         [".",".",".",".","8",".",".","7","9"]]
+from typing import List
 
-def is_valid(board, r, c, i):
-    for j in range(9):
-        if board[r][j] == str(i): # for row condition
-            return False
-        if board[j][c] == str(i): # for col condition
-            return False
-        if board[3 * (r // 3) + (j // 3)][3 * (c // 3) + (j % 3)] == str(i): # for sub-matrix condition
-            return False
-    return True
 
-def solveSudoku(board):
-    # running loop for every element of matrix
-    for r in range(9):
-        for c in range(9):
-
-            # only if the value at board[r][c] is not determined we will proceed
-            if board[r][c] == ".":
-                for i in range(1, 10):
-                    # some black_box function which tells us whether we can put number i at board[r][c] position
-                    if is_valid(board, r, c, i):
-                        board[r][c] = str(i)
-
-                        # if we found a solution we return true else we backtrack and make board[r][c] = '.'
-                        if solveSudoku(board) == True:
-                            return True
-                        else:
-                            board[r][c] = '.'
-                # we will return false if we tried all (1 -> 10) numbers and none of it results in satisfying all three sudoku condition
+class Solution:
+    def isValid(self, board, r, c, i):
+        for j in range(9):
+            if board[r][j] == str(i): # for row condition
                 return False
+            if board[j][c] == str(i): # for col condition
+                return False
+            if board[3 * (r // 3) + (j // 3)][3 * (c // 3) + (j % 3)] == str(i): # for sub-matrix condition
+                return False
+        return True
 
-    return True
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def backtrack(board):
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == '.':
+                        for value in range(1, 10):
+                            if self.isValid(board, r, c, value):
+                                board[r][c] = str(value)
+                                if backtrack(board):
+                                    return True
+                                else:
+                                    board[r][c] = '.'
+                        return False
+            return True
 
-solveSudoku(board=board)
-
-print(*board)
+        backtrack(board)
