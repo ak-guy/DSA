a5b593b36388f8e2f90e54c467df8a1ecfe0f9d9
Arpit <arpitkumar3203@gmail.com>
2024-05-01 08:33:09 +0530

    added vertical order traversal

--- Binary Tree/vertical_order_traversal.py
+++ Binary Tree/vertical_order_traversal.py
@@ -1,11 +1,6 @@
 # Definition for a binary tree node.
-from __future__ import annotations
-
-from collections import defaultdict
-from collections import deque
-from typing import List
-from typing import Optional
-
+from collections import deque, defaultdict
+from typing import Optional, List
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -13,13 +8,11 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
 class Solution:
-    def verticalTraversal(self, root: TreeNode | None) -> list[list[int]]:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         q = deque()
-        q.append((root, 0, 0))  # root_node, col, height
-        # {main_key : {sub_key: [values]}}
-        mappings = defaultdict(lambda: defaultdict(list))
+        q.append((root, 0 , 0)) # root_node, col, height
+        mappings = defaultdict(lambda: defaultdict(list)) # {main_key : {sub_key: [values]}}
         leftmost_col = 10000001
         rightmost_col = -10000001
         while q:
@@ -31,7 +24,7 @@ class Solution:
                     mappings[col][height].append(node.val)
                     q.append((node.left, col-1, height+1))
                     q.append((node.right, col+1, height+1))
-
+        
         total_cols = rightmost_col - leftmost_col + 1
         res = [[] for i in range(total_cols)]
 
@@ -39,5 +32,5 @@ class Solution:
             for new_key, new_val in val.items():
                 some_index = abs(leftmost_col) + key
                 res[some_index].extend(sorted(new_val))
-
+                
         return res
