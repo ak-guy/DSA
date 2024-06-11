3ed4b635897f00ddd515169b9ff64961284ceb45
Ankit Kumar <ak022856@gmail.com>
2024-06-11 14:23:33 +0530

    added intro to graph

---
+++ Graph/01_Intro/bfs_dfs.py
@@ -0,0 +1,60 @@
+'''this module takes input from user and creates a graph(Directed/Undirected Cyclic/Acyclic Graph with single component),
+   and then takes a starting node and traverses on that graph in BFS manner
+'''
+from collections import defaultdict, deque
+from typing import List
+
+def createGraph(val1: int, val2: int, graph_type: int, gp: defaultdict):
+    gp[val1].append(val2)
+    if graph_type == 1:
+        gp[val2].append(val1)
+
+def bfsTraversal(startnode: int, gp: defaultdict) -> List[int]:
+    q = deque()
+    q.append(startnode)
+    res = []
+    visited = set()
+    visited.add(startnode)
+    while q:
+        node = q.popleft()
+        res.append(node)
+        for child_node in gp[node]:
+            if child_node not in visited:
+                visited.add(child_node)
+                q.append(child_node)
+            
+    return res
+
+def dfsTraversal(startnode: int, gp: defaultdict) -> List[int]:
+    res = []
+    visited = set()
+    
+    def helper(node):
+        res.append(node)
+        visited.add(node)
+        for child_node in gp[node]:
+            if child_node not in visited:
+                helper(child_node)
+
+    helper(startnode)
+    return res
+
+if __name__ == '__main__':
+    graph_type = int(input("Enter integer value for graph type directed(0) / undirected(1) : "))
+    n = int(input("Enter the number of edges : "))
+
+    gp = defaultdict(list)
+    for _ in range(n):
+       val1, val2 = map(int, input().split(' '))
+       createGraph(val1, val2, graph_type, gp)
+
+    print(gp) # for debugging
+
+    startnode = int(input("Enter starting node : "))
+    if startnode not in gp.keys():
+        startnode = int(input("Entered startnode does not exist in graph, Please enter correct starting node : "))
+
+    final_bfs_traversal = bfsTraversal(startnode, gp)
+    final_dfs_traversal = dfsTraversal(startnode, gp)
+    print(*final_bfs_traversal)
+    print(*final_dfs_traversal)
