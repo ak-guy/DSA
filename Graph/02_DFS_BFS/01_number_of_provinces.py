1970357da90b22207e18f504ba6f9ba3388e80a4
Ankit Kumar <ak022856@gmail.com>
2024-06-11 17:07:40 +0530

    added number of provinces

---
+++ Graph/02_DFS_BFS/01_number_of_provinces.py
@@ -0,0 +1,27 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def traverseAndMarkVisitedCities(self, visited: set, city:int, isConnected: List[List[int]], n: int) -> None:
+        q = deque()
+        visited.add(city)
+        q.append(city)
+        while q:
+            current_city = q.popleft()
+            for i in range(n):
+                if isConnected[current_city-1][i] == 1 and i+1 not in visited:
+                    q.append(i+1)
+                    visited.add(i+1)
+
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = set()
+        provinces = 0
+        
+        for city in range(1, n+1):
+            if city not in visited:
+                self.traverseAndMarkVisitedCities(visited, city, isConnected, n)
+                provinces += 1
+
+        return provinces
