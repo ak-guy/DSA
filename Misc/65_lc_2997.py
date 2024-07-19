d2e76f5751b894eb1d11016dea49ffc8d735bba0
Arpit <arpitkumar3203@gmail.com>
2024-07-19 23:53:51 +0530

    better logic added

--- Misc/65_lc_2997.py
+++ Misc/65_lc_2997.py
@@ -1,11 +1,13 @@
 from typing import List
 
+# Method 1:
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         total_xor = 0
         for num in nums:
             total_xor ^= num
-            
+        
+        # finding mismatching bits bw total_xor and k using string (Not good)
         bit_repr_of_total_xor = bin(total_xor)[2:]
         bit_repr_of_k = bin(k)[2:]
         n = len(bit_repr_of_total_xor)
@@ -21,4 +23,14 @@ class Solution:
             if bit_repr_of_total_xor[i] != bit_repr_of_k[i]:
                 res += 1
 
+        return res
+    
+# Method 2: Less line of codes and better use of bitwise knowledge
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        total_xor = 0
+        for num in nums:
+            total_xor ^= num
+            
+        res = bin(total_xor ^ k)[2:].count('1')
         return res
