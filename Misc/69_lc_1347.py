0c32e0ae0449941f1ca77ff165f6554aaa1fa3be
Arpit <arpitkumar3203@gmail.com>
2024-07-21 13:38:41 +0530

    added 1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ Misc/69_lc_1347.py
@@ -0,0 +1,19 @@
+'''
+1347. Minimum Number of Steps to Make Two Strings Anagram
+'''
+
+from collections import Counter
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        res = 0
+        for s_key in counter_s.keys():
+            s_key_count = counter_s[s_key]
+            s_key_count_in_t = counter_t.get(s_key, 0)
+            if s_key_count >= s_key_count_in_t:
+                res += s_key_count - s_key_count_in_t
+
+        return res
