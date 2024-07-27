e7c49834b292c861b51072cb0958146013c158a8
Arpit <arpitkumar3203@gmail.com>
2024-07-27 12:22:53 +0530

    2785. Sort Vowels in a String

---
+++ Misc/75_lc_2785.py
@@ -0,0 +1,25 @@
+'''
+2785. Sort Vowels in a String
+'''
+
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels = frozenset(['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'])
+
+        vowels_in_s = []
+        for char in s:
+            if char in vowels:
+                vowels_in_s.append(char)
+        
+        vowels_in_s.sort()
+
+        res = ''
+        i = 0
+        for ind, char in enumerate(s):
+            if char in vowels:
+                res += vowels_in_s[i]
+                i += 1
+            else:
+                res += char
+        
+        return res
