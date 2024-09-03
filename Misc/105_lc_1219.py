4f443876f2ec1bb06a60845755d956b02e854ad7
Arpit <arpitkumar3203@gmail.com>
2024-09-03 20:53:10 +0530

    added 1219. Path with Maximum Gold

---
+++ Misc/105_lc_1219.py
@@ -0,0 +1,32 @@
+'''
+1219. Path with Maximum Gold
+'''
+from typing import List 
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        def backtrack(r,c):
+            temp = grid[r][c]
+            res = temp
+            grid[r][c] = 0
+            directions = [[0,1],[1,0],[-1,0],[0,-1]]
+            
+            for direction in directions:
+                row = r + direction[0]
+                col = c + direction[1]
+                if row>=0 and col>=0 and row<n and col<m  and grid[row][col] != 0:
+                    res = max(res, temp + backtrack(row, col))
+
+            # backtrack
+            grid[r][c] = temp
+            return res
+
+        n = len(grid)
+        m = len(grid[0])
+        res = 0
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] != 0:
+                    possibleRes = backtrack(r,c)
+                    res = max(res, possibleRes)
+        
+        return res
--- test.java
+++ test.java
@@ -1,8 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 3;
-        double div = x/y;
-        System.out.println(div);
+        
     }
 }
