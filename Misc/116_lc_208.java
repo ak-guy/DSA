ef33b4fc77b09bcbe0b991a69e4ee6f08d6a5349
Arpit <arpitkumar3203@gmail.com>
2024-09-08 12:30:55 +0530

    added 208. Implement Trie (Prefix Tree)

---
+++ Misc/116_lc_208.java
@@ -0,0 +1,83 @@
+package Misc;
+
+/*
+ * 208. Implement Trie (Prefix Tree)
+ */
+
+class Node {
+    public Node[] node;
+    private boolean endFlag; // by default false
+
+    public Node() {
+        this.node = new Node[26];
+    }
+
+    public boolean containsKey(char c) {
+        return node[c-'a'] != null;
+    }
+
+    public void putKey(char c) {
+        node[c-'a'] = new Node();
+    }
+
+    public Node getChar(char c) {
+        return node[c-'a'];
+    }
+
+    public boolean getEndFlag() {
+        return this.endFlag;
+    }
+
+    public void setEndFlag(boolean val) {
+        this.endFlag = val;
+    }
+}
+
+class Trie {
+    private Node root;
+    public Trie() {
+        root = new Node();
+    }
+    
+    public void insert(String word) {
+        Node node = root; // getting a new node to iterate otherwise main root will be shifted  
+        for (char ch: word.toCharArray()) {
+            // if char already exists in root then we only need to shift reference
+            if (!node.containsKey(ch)) {
+                node.putKey(ch);
+            }
+            node = node.getChar(ch); // shifting reference node
+        }
+        node.setEndFlag(true);
+    }
+    
+    public boolean search(String word) {
+        Node node = root;
+        for (char ch: word.toCharArray()) {
+            if (!node.containsKey(ch)) {
+                return false;
+            }
+            node = node.getChar(ch);
+        }
+        return node.getEndFlag();
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node node = root;
+        for (char ch: prefix.toCharArray()) {
+            if (!node.containsKey(ch)) {
+                return false;
+            }
+            node = node.getChar(ch);
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
