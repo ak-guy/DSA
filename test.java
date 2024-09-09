2c21b8f7e5d9717308dda7c645dc77c06f575379
Arpit <arpitkumar3203@gmail.com>
2024-09-09 18:27:48 +0530

    added 1609. Even Odd Tree

--- test.java
+++ test.java
@@ -1,54 +1,7 @@
-import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-class CBTInserter {
-    private TreeNode root;
-    public CBTInserter(TreeNode root) {
-        this.root = root;
-    }
-    
-    public int insert(int val) {
-        if (root == null) {
-            root = new TreeNode(val);
-            return val;
-        }
-
-        TreeNode tempRoot = root;
-        Queue<TreeNode> q = new LinkedList<>(); 
-        q.offer(tempRoot);
-        while (!q.isEmpty()) {
-            int qSize = q.size();
-            for (int i=0; i<qSize; i++)  {
-                TreeNode node = q.remove();
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
-
-                if (node.left == null) {
-                    node.left = new TreeNode(val);
-                    return node.val;
-                }
-                if (node.right == null) {
-                    node.right = new TreeNode(val);
-                    return node.val;
-                }
-            }
-        }
-        return 0;
-    }
-    
-    public TreeNode get_root() {
-        return root;
+public class test {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x & 1;
+        System.out.println(y);
     }
 }
