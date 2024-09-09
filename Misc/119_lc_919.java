874aa1218b86cf1fcbeaa70379f9ffd4fafaca2f
Arpit <arpitkumar3203@gmail.com>
2024-09-09 16:47:36 +0530

    added optimized approach

--- Misc/119_lc_919.java
+++ Misc/119_lc_919.java
@@ -61,3 +61,42 @@ class CBTInserter {
 }
 
 
+// Optimized approach
+class CBTInserter2 {
+    List<TreeNode> listRep;
+    public CBTInserter2(TreeNode root) {
+        this.listRep = new ArrayList<>();
+        this.listRep.add(root);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            for (int i=0; i<qSize; i++) {
+                TreeNode node = q.remove();
+                if (node.left != null) {
+                    q.offer(node.left);
+                    this.listRep.add(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                    this.listRep.add(node.right);
+                }
+            }
+        }
+    }
+    
+    public int insert(int val) {
+        TreeNode node = new TreeNode(val);
+        listRep.add(node);
+        int listRepSize = listRep.size();
+        TreeNode parent = listRep.get(listRepSize/2 - 1);
+        if (parent.left == null) parent.left = node;
+        else if (parent.right == null) parent.right = node;
+
+        return parent.val; 
+    }
+    
+    public TreeNode get_root() {
+        return this.listRep.get(0);
+    }
+}
