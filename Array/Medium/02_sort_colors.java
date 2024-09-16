d4698d70bac8f4f563b98bad003c0678a8cb8145
Arpit <arpitkumar3203@gmail.com>
2024-09-16 23:59:20 +0530

    added algorithm for sort color

--- Array/Medium/02_sort_colors.java
+++ Array/Medium/02_sort_colors.java
@@ -6,6 +6,16 @@ package Array.Medium;
 
 class Solution {
     public void sortColors(int[] nums) {
+        /*
+        The idea behind the algorithm is to keep all the 0s before the low pointer,
+        all the 2s after the high pointer, and all the 1s between the low and high 
+        pointers. The algorithm moves the mid pointer through the array, comparing 
+        the value at each position with 1. If the value is 0, the element is swapped
+        with the element at the low pointer, and the low and mid pointers are 
+        incremented. If the value is 2, the element is swapped with the element at
+        the high pointer, and the high pointer is decremented. If the value is 1, 
+        the mid pointer is simply incremented.
+        */
         int start = 0;
         int end = nums.length-1;
         int curr = 0;
