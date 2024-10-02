ab408d55132089b2971b963b3b8dde5b2354eabf
Arpit <arpitkumar3203@gmail.com>
2024-10-02 13:08:37 +0530

    added Binary Search solution

--- Misc/138_lc_378.py
+++ Misc/138_lc_378.py
@@ -20,3 +20,33 @@ class Solution:
                     heapLen -= 1
         
         return -1 * (heapq.heappop(pq))
+    
+# Method - 2 : Binary Search
+# Binary Search
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n, m = len(matrix), len(matrix[0])
+
+        def smallerOrEqualCount(val):
+            res = 0
+            colEnd = m-1
+            for r in range(n):
+                while colEnd >= 0 and matrix[r][colEnd] > val:
+                    colEnd -= 1
+                res += (colEnd+1)
+            
+            return res
+
+        # search range
+        start, end = matrix[0][0], matrix[n-1][m-1]
+        ans = 0
+        while start <= end:
+            toSearch = (end + start) // 2
+            if smallerOrEqualCount(toSearch) >= k:
+                ans = toSearch
+                end = toSearch-1
+            else:
+                start = toSearch+1
+
+        return ans
+        
